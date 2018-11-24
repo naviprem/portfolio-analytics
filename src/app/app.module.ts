34ee215f4848c96f06fa2677287ea1470c6c9bd3
naviprem <naveena.premkrishna@gmail.com>
2018-11-23 17:38:49 -0800

    routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HiLoComponent } from './hi-lo/hi-lo.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GenericErrorComponent } from './generic-error/generic-error.component';
+
+
+const routes: Routes = [
+  {
+    path: 'hi-lo',
+    component: HiLoComponent,
+    data: {title: 'Login Redirect'}
+  },
+  {
+    path: 'error-page',
+    component: GenericErrorComponent,
+    data: {title: 'Error Page'}
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '*',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    data: {title: 'Page Not Found'}
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HiLoComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    GenericErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
